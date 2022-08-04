2115e3bf1d1a8f26c44684fd0f6000dc00ca2aed
FERNANDO HENRIQUE FERNANDES DA SILVA <f3rnandorj10@gmail.com>
2022-08-04 08:52:17 -0300

    Aplication bcryptjs
    Criptografando a senha para o banco de dados.

--- src/app/models/user.js
+++ src/app/models/user.js
@@ -1,11 +1,14 @@
 import Sequelize, {Model} from 'sequelize';
 
+import bcrypt from 'bcryptjs';
+
 class User extends Model {
   static init(sequelize) {
     super.init (
       {                                  // so entram as que o usuario preenche
         name: Sequelize.STRING,
         email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
         password_hash: Sequelize.STRING,
         provider: Sequelize.BOOLEAN,
       },
@@ -13,6 +16,16 @@ class User extends Model {
         sequelize,
       }
     );
+
+    this.addHook('beforeSave', async (User) => {
+      if (User.password){
+        User.password_hash = await bcrypt.hash(User.password, 8);
+      }
+
+    });
+
+    return this;
+
   }
 }
 
