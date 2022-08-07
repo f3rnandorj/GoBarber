be12ce94e368b30fa72bae8d647cf7481410fd8c
FERNANDO HENRIQUE FERNANDES DA SILVA <f3rnandorj10@gmail.com>
2022-08-07 15:02:43 -0300

    Creating authentication of user
    Criação da autenticação entre os dados de usuario e do banco de dados.

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -19,4 +19,5 @@ class UserController {
   }
 }
 
+
 export default new UserController();
--- src/app/models/user.js
+++ src/app/models/user.js
@@ -17,15 +17,19 @@ class User extends Model {
       }
     );
 
-    this.addHook('beforeSave', async (User) => {
-      if (User.password){
-        User.password_hash = await bcrypt.hash(User.password, 8);
+    this.addHook('beforeSave', async (user) => {
+      if (user.password){
+        user.password_hash = await bcrypt.hash(user.password, 8);
       }
 
     });
 
     return this;
 
+  }
+
+  checkPassword(password) {
+    return bcrypt.compare(password, this.password_hash);
   }
 }
 
