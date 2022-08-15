fbf2166a3cdcef75595f2d1216d441ed59db82e4
FERNANDO HENRIQUE FERNANDES DA SILVA <f3rnandorj10@gmail.com>
2022-08-15 08:21:44 -0300

    Creating and relating appointment table
    Criação da migration de appointment, seguido da criação do model e aplicação no index do database.

---
+++ src/app/models/Appointment.js
@@ -0,0 +1,25 @@
+import Sequelize, { Model } from 'sequelize';
+
+class File extends Model {
+  static init(sequelize) {
+    super.init (
+      {                                  // so entram as que o usuario preenche
+        date: Sequelize.DATE,
+        canceled_at: Sequelize.DATE,
+      },
+      {
+        sequelize,
+      }
+    );
+
+    return this;
+
+  }
+
+  static associate (models) {
+    this.belongsTo(models.User, { foreignKey: 'user_id', as: 'user' });
+    this.belongsTo(models.User, { foreignKey: 'provider_id', as: 'provider' });
+  }
+}
+
+export default File;
