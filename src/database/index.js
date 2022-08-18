17288f69f3d40f630ec8e5be39eb1fe1a188c730
FERNANDO HENRIQUE FERNANDES DA SILVA <f3rnandorj10@gmail.com>
2022-08-18 19:35:57 -0300

    Adding and config MongoDB
    Ajustei a versão do mongoose, conectei o API ao Mongo através do index, criei a parte que faz referencia e tipifica o banco não relacionado, através do Notification.js.

--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,5 @@
 import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
 
 import User from '../app/models/User';
 import File from '../app/models/File';
@@ -11,8 +12,10 @@ const models = [User, File, Appointment];
 class Database {
   constructor() {
     this.init();
+    this.mongo();
   }
 
+  // metodo init()
   init() {
     this.connection = new Sequelize(databaseConfig);
 
@@ -21,6 +24,18 @@ class Database {
       .map(
         (model) => model.associate && model.associate(this.connection.models)
       );
+  }
+
+  // metodo mongo()
+  mongo() {
+    this.mongoConnection = mongoose.connect(
+      'mongodb://localhost:27017/gobarber',
+      {
+        useNewUrlParser: true,
+        useFindAndModify: true,
+        useUnifiedTopology: true,
+      }
+    );
   }
 }
 
