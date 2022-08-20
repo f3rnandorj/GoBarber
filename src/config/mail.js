e68cc4d2746b0206a93c3de5279df7a02ffc8267
FERNANDO HENRIQUE FERNANDES DA SILVA <f3rnandorj10@gmail.com>
2022-08-20 11:23:11 -0300

    Send an email from provider
    Enviando um e-mail para o provedor informando sobre o cancelamento de um serviço.

---
+++ src/config/mail.js
@@ -0,0 +1,21 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'cbe3894ae1407e',
+    pass: 'bfbdfe078c1b37',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
+
+/*
+Serviços de envio de email
+Amazon SES
+Mailgun
+Sparkpost
+Mandril (Mailchimp)
+Mailtrap.io ( somente para desenvolvimento )
+*/
