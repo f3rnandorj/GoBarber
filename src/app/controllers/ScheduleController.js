35f92513001af4656c09345108beb2d8f868024d
FERNANDO HENRIQUE FERNANDES DA SILVA <f3rnandorj10@gmail.com>
2022-11-03 19:30:13 -0300

    Adding details
    Adicionado alguns retornos necessarios para a aplicação.

--- src/app/controllers/ScheduleController.js
+++ src/app/controllers/ScheduleController.js
@@ -24,6 +24,13 @@ class ScheduleController {
         date: {
           [Op.between]: [startOfDay(parsedDate), endOfDay(parsedDate)],
         },
+        include: [
+          {
+            model: User,
+            as: 'user',
+            attribute: ['name'],
+          },
+        ],
       },
       order: ['date'],
     });
